aed54ee77a1821c3168bdd0129c5206e4bd5cc7c
DAN92DE <danielweber1992@outlook.de>
2023-05-28 18:19:05 +0200

    exercise detail page & accordion component

---
+++ components/accordion.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{ title: string, isOpen?: boolean, rootClasses?: string }>();
+const showHeader = ref(props.isOpen);
+const toggleHeader = () => showHeader.value = !showHeader.value;
+</script>
+
+<template>
+  <div :class="rootClasses">
+    <div class="bg-slate-700 p-4 flex justify-between">
+      <p class="m-0 font-bold text-white">{{ title?.toUpperCase() }}</p>
+      <p class="m-0 font-bold text-white hover:cursor-pointer hover:opacity-50" @click="toggleHeader">
+        <Icon v-if="showHeader" name="mdi:chevron-double-up"></Icon>
+        <Icon v-else name="mdi:chevron-double-down"></Icon>
+      </p>
+    </div>
+    <div v-show="showHeader" class="p-6 bg-slate-50">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/exercise/[exercise].vue
+++ pages/exercise/[exercise].vue
@@ -10,12 +10,35 @@ onMounted( () => {
     navigateTo('/explore');
   }
 })
-
 </script>
 
 <template>
-  <h1>exercise {{ route.params?.exercise }} works...</h1>
-  <pre>{{ currentExercise }}</pre>
+  <div>
+    <div class="bg-slate-50 mb-6 grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <img class="w-full" :src="currentExercise?.image" :alt="currentExercise?.name" />
+      </div>
+      <div class="p-6">
+        <h1 class="text-3xl font-bold md:mb-6">{{ currentExercise?.name }}</h1>
+        <p>{{ currentExercise?.description }}</p>
+      </div>
+    </div>
+
+    <Accordion title="Exercise Images" :is-open="true" root-classes="mb-6">
+      <div class="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-5 gap-4">
+        <img v-for="img of currentExercise?.images" :src="img" :alt="currentExercise.name"
+             class="w-full shadow-sm hover:opacity-50 hover:cursor-pointer hover:border-l-2 hover:border-slate-700" />
+      </div>
+    </Accordion>
+
+    <Accordion title="Exercise Videos" :is-open="false">
+      <div class="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-5 gap-4">
+        <img v-for="img of currentExercise?.images" :src="img" :alt="currentExercise.name"
+             class="w-full shadow-sm hover:opacity-50 hover:cursor-pointer hover:border-l-2 hover:border-slate-700" />
+      </div>
+    </Accordion>
+  </div>
+
 </template>
 
 <style scoped>
