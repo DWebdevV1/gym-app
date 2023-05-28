94043ad2f935d3c5f5ad1d540f8372e995e712ab
DAN92DE <danielweber1992@outlook.de>
2023-05-28 15:32:46 +0200

    handle own state for now

--- pages/exercise/[exercise].vue
+++ pages/exercise/[exercise].vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 const route = useRoute();
+const currentExercise = ref(null);
+
+onMounted( () => {
+  const { getOwnState } = useOwnState();
+  currentExercise.value = getOwnState();
+
+  if (!currentExercise.value) {
+    navigateTo('/explore');
+  }
+})
+
 </script>
 
 <template>
   <h1>exercise {{ route.params?.exercise }} works...</h1>
+  <pre>{{ currentExercise }}</pre>
 </template>
 
 <style scoped>
