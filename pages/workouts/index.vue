154d8af70a76e18d4e2f551ede9a59d909b44bc2
DAN92DE <danielweber1992@outlook.de>
2023-06-04 13:21:22 +0200

    workout exercises layout

--- pages/workouts/index.vue
+++ pages/workouts/index.vue
@@ -36,11 +36,16 @@ const navigateToExercise = () => {
 
   <div>
     <Headline title="Last visited Exercises" icon="ic:baseline-remove-red-eye"></Headline>
-    <div v-for="() of 5" class="bg-slate-50 mb-6 p-6 flex justify-between font-bold hover:cursor-pointer hover:opacity-50"
+    <div v-for="() of 5" class="bg-slate-50 mb-6 font-bold hover:cursor-pointer hover:opacity-50"
          @click="navigateToExercise">
-      <span>My Workout Name</span>
-      <span>04/06/2023</span>
-      <Icon name="material-symbols:arrow-circle-right"></Icon>
+      <div class="flex items-center">
+        <img class="h-32" src="https://placehold.co/400x400" alt="exercise" />
+        <div class="p-6 flex flex-col sm:flex-row items-end sm:justify-between w-full">
+          <span>My Workout Name</span>
+          <span>04/06/2023</span>
+          <Icon name="material-symbols:arrow-circle-right"></Icon>
+        </div>
+      </div>
     </div>
   </div>
 </template>
