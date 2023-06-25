8820a1f8d5cff81837a6e0d7ebbf74b444b25aba
DAN92DE <danielweber1992@outlook.de>
2023-06-25 12:32:08 +0200

    last visited exercises check

--- server/api/last-visited-exercises.post.ts
+++ server/api/last-visited-exercises.post.ts
@@ -3,7 +3,9 @@ import data from '~/server/data/PLACEHOLDER_DATA.json';
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
 
-    if (body?.id && body?.name && body?.visitedOn && body?.image) {
+    const exAlreadyExists = !!(data.lastVisitedExercises.find(ex => ex.id === body.id));
+
+    if (!exAlreadyExists && body?.id && body?.name && body?.visitedOn && body?.image) {
         if (data.lastVisitedExercises.length >= 5) {
             data.lastVisitedExercises.shift();
         }
